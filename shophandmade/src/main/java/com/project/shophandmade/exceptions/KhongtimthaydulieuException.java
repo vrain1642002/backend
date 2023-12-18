298f4713eb52e5382241fe2f5af0a47a1f6b119c
/vrain <98318713+vrain1642002@users.noreply.github.com>
2023-12-19 01:53:22 +0700

    delte

--- shophandmade/src/main/java/com/project/shophandmade/exceptions/KhongtimthaydulieuException.java
+++
@@ -1,7 +0,0 @@
-package com.project.shophandmade.exceptions;
-
-public class KhongtimthaydulieuException extends Exception{
-    public KhongtimthaydulieuException(String message) {
-        super(message);
-    }
-}
